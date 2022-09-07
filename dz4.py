57e89f6d8a0a13ad0de79530b8627f4fe0a2c42c
VyacheslavShrot <slavic.shrot@gmail.com>
2022-09-07 09:50:51 +0300

    dz1

--- dz4.py
+++ dz4.py
@@ -1,13 +1,15 @@
 number1 = float(input("Type first number:"))
 oper = input("Type operation:")
 number2 = float(input("Type second number:"))
-if oper == "+":
-    print(number1 + number2)
-elif oper == "-":
-    print(number1 - number2)
-elif oper == "*":
-    print(number1 * number2)
-elif oper == "/":
-    print(number1 / number2)
-else:
-    print("Error")
+
+if number2 != 0:
+    if oper == "+":
+        print(number1 + number2)
+    elif oper == "-":
+        print(number1 - number2)
+    elif oper == "*":
+        print(number1 * number2)
+    elif oper == "/":
+        print(number1 / number2)
+    else:
+        print("Error")

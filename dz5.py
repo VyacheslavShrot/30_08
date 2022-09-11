b5c970fa41eb51762f114bd1085aee29dae3d26c
VyacheslavShrot <slavic.shrot@gmail.com>
2022-09-12 01:04:06 +0300

    1

--- dz5.py
+++ dz5.py
@@ -1,14 +1,11 @@
-li = ["cow", "1", "cat", "2", "dog", "3"]
-animals = []
-num = []
-for n in li:
-    num.append(n) if n.isnumeric() else animals.append(n)
-print(animals)
-print(num)
-print(li)
-
-
-
-
-
-
+lst = [1, 2, 3, 4, 5, 6]
+lst1 = []
+lst2 = []
+if len(lst) == 0:
+    print([lst] * 2)
+elif len(lst) % 2 == 0:
+    middle_of_list = len(lst) // 2
+    print(middle_of_list)
+    x = lst.pop()
+    print(lst)
+    print(x)

d16205465c466aa96adb667d0349aee15184b2de
VyacheslavShrot <slavic.shrot@gmail.com>
2022-09-12 22:19:29 +0300

    1

--- dz5.py
+++ dz5.py
@@ -3,7 +3,7 @@ lst1 = []
 lst2 = []
 if len(lst) == 0:
     print([lst] * 2)
-elif len(lst) % 2 == 0 and len(lst) == 6:
+elif len(lst) % 2 == 0:
     middle_of_list = len(lst) // 2
     print(middle_of_list)
     x = lst.pop()

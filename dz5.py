e50d7b54f11494c319c0b3bcccc5506d13781d1a
VyacheslavShrot <slavic.shrot@gmail.com>
2022-09-15 11:46:53 +0300

    1

--- dz5.py
+++ dz5.py
@@ -1,11 +1,9 @@
-lst = [1, 2, 3, 4, 5, 6]
-lst1 = []
-lst2 = []
+lst = [1, 2, 3, 4, 5]
 if len(lst) == 0:
     print([lst] * 2)
 elif len(lst) % 2 == 0:
     middle_of_list = len(lst) // 2
-    print(middle_of_list)
-    x = lst.pop()
-    print(lst)
-    print(x)
+    print([lst[:middle_of_list]] + [lst[middle_of_list:]])
+elif len(lst) % 2 != 0:
+    middle_of_list = (len(lst) // 2) + 1
+    print([lst[:middle_of_list]] + [lst[middle_of_list:]])

f2f6fe9d14ba73002a2623fd0753c74b0a4ff3b3
VyacheslavShrot <slavic.shrot@gmail.com>
2022-09-18 16:20:05 +0300

    1

--- dz7.py
+++ dz7.py
@@ -1,10 +1,11 @@
-lst = [1, 0, 2, 0, 3, 0, 0]
-while 0 in lst:
-    for n in lst:
-        if n == 0:
-            a = lst.remove(0)
+lst = [1, 0, 2, 0, 3, 0, 0, 0]
+for n in lst:
+    if n == 0:
+        a = lst.remove(0)
+        while 0 in lst:
             print(lst + [n])
             break
+
 
 
 

1341a41c04e2af83f812189e3397f929a6a83209
VyacheslavShrot <slavic.shrot@gmail.com>
2022-10-11 11:33:23 +0300

    1

---
+++ dz16.py
@@ -0,0 +1,14 @@
+#say_hi(input("Name:"),int(input("Years:")))
+#say_hi = ("Alex", 32)
+
+
+
+def say_hi(str,int):
+
+ print(f"Hi.My name is {str} and I'm {int} years old.")
+
+say_hi(input("Name:"), int(input("Years:")))
+
+
+
+#say_hi = lambda name, age:   "Hi. My name is {"

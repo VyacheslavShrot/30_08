2901c92fc858eeaea5d1be8f4d96db6df743aeb9
VyacheslavShrot <slavic.shrot@gmail.com>
2022-10-11 20:04:16 +0300

    1

---
+++ dz18.py
@@ -0,0 +1,14 @@
+def second_index(text, symbol):
+    if text.find(symbol) == True:
+        if text.find(symbol, text.find(symbol) + 1) == True:
+            l = (text.find(symbol))
+            text = text.find(symbol, l + 1)
+            return text
+
+
+
+
+
+print(second_index("sims", 's'))
+print(second_index("find the river", 'e'))
+print(second_index("hi", 'h'))

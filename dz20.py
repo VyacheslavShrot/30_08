856be6bb6635c6bff62a5194bb1e0d1ac60923ad
VyacheslavShrot <slavic.shrot@gmail.com>
2022-10-14 19:35:52 +0300

    1

---
+++ dz20.py
@@ -0,0 +1,16 @@
+def add_one(lst: list) -> list:
+    a = ''
+    for n in lst:
+        a += str(n)
+    a = int(a) + 1
+    b = []
+    for i in str(a):
+        b.append(int(i))
+    return b
+
+
+
+print(add_one([1, 2, 3, 4]))
+print(add_one([0]))
+print(add_one([9, 9, 9, 9]))
+print(add_one([1, 0]))

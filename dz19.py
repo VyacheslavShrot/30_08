dd4060ea4514f6f433331c39ff15c1d59fc12f5f
VyacheslavShrot <slavic.shrot@gmail.com>
2022-10-18 18:42:23 +0300

    1

--- dz19.py
+++ dz19.py
@@ -5,6 +5,8 @@ b = random.randint(0, 30)
 
 list1, list2 = list(), list()
 
+list3 = list()
+
 for n in range(0, a):
     if n % 3 == 0:
         list1.append(n)
@@ -21,9 +23,23 @@ for n in range(0, b):
 
 set1 = set()
 set1.update(list1, list2)
+list1 = set(list1)
+list2 = set(list2)
 
-list3 = list(set1)
+
+for n in list1:
+    if n in list2:
+        if n % 5 == 0:
+            list3.append(n)
+
+for n in list2:
+    if n in list1:
+        if n % 3 == 0:
+            list3.append(n)
+
 
 print(list1)
 print(list2)
+
+list3 = set(list3)
 print(list3)

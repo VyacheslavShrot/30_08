09647c503c1f854678582a0a8aa7955e5a3210f7
VyacheslavShrot <slavic.shrot@gmail.com>
2022-10-18 19:56:45 +0300

    1

---
+++ dz24.py
@@ -0,0 +1,25 @@
+from typing import Union
+
+def difference(*args) -> Union[int, float]:
+    if len(args) == 0:
+        return 0
+
+    return float(max(args) - min(args))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(difference(1, 2, 3))
+print(difference(5, -5))
+print(difference(10.2, -2.2, 0, 1.1, 0.5))
+print(difference())
